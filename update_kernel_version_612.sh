0d0a9fa84a8429288119fff1ccf5d8e487204957
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-06-16 15:55:20 +0900

    DEV: kernel update script: Use renesas-lts branch only
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- update_kernel_version_612.sh
+++ update_kernel_version_612.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd `dirname $0` && pwd)
 cd $SCRIPT_DIR
 API_URL="https://api.github.com/repos/morimoto/linux/branches"
 
-LATEST_BRANCH=$(curl -s ${API_URL} | jq ".[].name" | grep sparrow-hawk | tail -1)
+LATEST_BRANCH=$(curl -s ${API_URL} | jq ".[].name" | grep sparrow-hawk | grep renesas-lts | tail -1)
 CURRENT_BRANCH=$(cat recipes-kernel/linux/kernel_6.12.inc | grep BRANCH | awk '{print $3}')
 
 if [[ "$LATEST_BRANCH" != "$CURRENT_BRANCH" ]]; then
